3af85bbd3764dd389f6fcf126b9d2692459dcc59
Shang Enliang <lttclaw@qq.com>
2019-01-02 17:56:05 +0800

    gradle基本配置，utils相关类

---
+++ app/src/main/java/com/ycy/baseapp/base/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.ycy.baseapp.base;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity {
+}
---
+++ app/src/main/java/com/ycy/baseapp/base/MainApplication.java
@@ -0,0 +1,12 @@
+package com.ycy.baseapp.base;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
